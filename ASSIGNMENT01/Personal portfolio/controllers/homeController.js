7e91e0e93b690b8cfcea95b0883210376e966c03
Vishal <200562508@student.georgianc.on.ca>
2025-02-23 15:55:09 -0500

    last edit!!

--- ASSIGNMENT01/Personal portfolio/controllers/homeController.js
+++ ASSIGNMENT01/Personal portfolio/controllers/homeController.js
@@ -3,22 +3,12 @@ const router = express.Router();
 
 // Home Page
 router.get('/', (req, res) => {
-  res.render('home', { title: 'Home' });
-});
-
-// About Page
-router.get('/about', (req, res) => {
-  res.render('about', { title: 'About Me' });
+  res.render('index', { title: 'Home' });
 });
 
 // Projects Page
 router.get('/projects', (req, res) => {
   res.render('projects', { title: 'Projects' });
-});
-
-// Contact Page
-router.get('/contact', (req, res) => {
-  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
--- ASSIGNMENT01/Personal portfolio/index.js
+++ ASSIGNMENT01/Personal portfolio/index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const path = require('path');
-const hbs = require('hbs');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Set up Handlebars as the view engine
+app.engine('hbs', exphbs.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Set up static files (CSS, JS, images)
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', require('./controllers/index'));
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
